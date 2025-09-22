ad1d10c22366dd0d5da59b7e96cf235c41328b05
infortecmov03 <infortecmov@gmail.com>
2025-09-22 21:19:15 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'religioes-app-v1';
+const urlsToCache = [
+    '/seu-repositorio/',
+    '/seu-repositorio/index.html',
+    '/seu-repositorio/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
